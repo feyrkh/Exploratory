1339b29b7d355f63b85c207682f7cf10715ca620
Kevin Hobbs <kevinhobbs@gmail.com>
2024-01-30 21:56:12 -0600

    All tutorials for relax mode, still need to save their state

--- pottery/tutorial/ZoomTutorial.gd
+++ pottery/tutorial/ZoomTutorial.gd
@@ -13,7 +13,7 @@ func _ready():
 	orig_desired_movement_time = desired_movement_time
 	play_animation()
 
-func on_moved_item(item):
+func on_moved_item(_item):
 	desired_movement_time -= 0.25
 	tutorial_step_progress.emit(1.0 - desired_movement_time / orig_desired_movement_time)
 	if desired_movement_time <= 0:
@@ -21,6 +21,25 @@ func on_moved_item(item):
 		tutorial_step_success.emit()
 
 func play_animation():
-	$AnimationPlayer.play("glue_tutorial")
-	await $AnimationPlayer.animation_finished
+	$Mouse.position = mouse_start
+	$Mouse.texture = preload("res://art/mouse_none.png")
+	$Piece1.scale = piece1_start
+	$Mouse.modulate = Color.WHITE
+	$E.modulate = HIDE_COLOR
+	$Q.modulate = HIDE_COLOR
+	tween = create_tween()
+	tween.tween_property($Mouse, "texture", preload("res://art/mouse_scroll_down.png"), 0).set_delay(0.5)
+	tween.tween_property($Piece1, "scale", piece1_start * 0.5, 2.0)
+	tween.tween_property($Mouse, "texture", preload("res://art/mouse_scroll_up.png"), 0).set_delay(0.5)
+	tween.tween_property($Piece1, "scale", piece1_start, 2.0)
+	tween.tween_property($Mouse, "texture", preload("res://art/mouse_none.png"), 0).set_delay(0.5)
+	tween.tween_property($Mouse, "texture", preload("res://art/mouse_none.png"), 0).set_delay(1.0)
+	tween.tween_property($Mouse, "modulate", HIDE_COLOR, 0)
+	tween.tween_property($Q, "modulate", Color.WHITE, 0)
+	tween.tween_property($Piece1, "scale", piece1_start * 0.5, 2.0)
+	tween.tween_property($Q, "modulate", HIDE_COLOR, 0)
+	tween.tween_property($E, "modulate", Color.WHITE, 0)
+	tween.tween_property($Piece1, "scale", piece1_start, 2.0)
+	tween.tween_property($E, "modulate", HIDE_COLOR, 0)
+	await tween.finished
 	call_deferred("play_animation")

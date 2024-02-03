7e4ac97ff621d8cef091d37d77f7e56984f0f9d9
Kevin Hobbs <kevinhobbs@gmail.com>
2024-02-03 00:28:19 -0600

    Release script

---
+++ release.bat
@@ -0,0 +1,9 @@
+grep 'config/version' project.godot
+set /p "id=Enter version number: "
+sed -i -b -E 's/config\/version=.+$/config\/version="%id%"/' project.godot
+del /S /Q ..\web_export\Keshiki\*
+..\GodotSteam-451.exe --headless --quit --export-debug Web ..\web_export\Keshiki\index.html
+powershell Compress-Archive ..\web_export\Keshiki\ ..\web_export\Keshiki\index.zip
+..\butler push ..\web_export\Keshiki\index.zip kevinhobbs/keshiki:web --userversion %id%
+
+set /p "done=Finished uploading"

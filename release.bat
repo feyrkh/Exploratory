9d3442c77ade9102a6dfa019d2eb3381fb039b8d
Kevin Hobbs <kevinhobbs@gmail.com>
2024-02-03 00:42:10 -0600

    release script also commits project version change

--- release.bat
+++ release.bat
@@ -2,6 +2,9 @@ git status --porcelain |findstr . && echo Commit your changes before releasing &
 grep 'config/version' project.godot
 set /p "id=Enter version number: "
 sed -i -b -E 's/config\/version=.+$/config\/version="%id%"/' project.godot
+git add project.godot
+git commit -m "Releasing %id%"
+git push
 del /S /Q ..\web_export\Keshiki\*
 ..\GodotSteam-451.exe --headless --quit --export-debug Web ..\web_export\Keshiki\index.html
 powershell Compress-Archive ..\web_export\Keshiki\ ..\web_export\Keshiki\index.zip

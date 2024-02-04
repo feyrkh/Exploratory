42fcb03464db8c9f372a3ce4239949ae83cd1ea6
Kevin Hobbs <kevinhobbs@gmail.com>
2024-02-03 18:55:23 -0600

    Prevent steam from being used in web mode

--- util/SteamMgr.gd
+++ util/SteamMgr.gd
@@ -1,9 +1,10 @@
 extends Node
 
 var steam_enabled := false
-var steam_username = Steam.getPersonaName()
 
 func _ready():
+	if !OS.has_feature("steam"):
+		return
 	var initialize_response: Dictionary = Steam.steamInitEx( true, 480 )
 	print("Did Steam initialize?: %s " % initialize_response)
 	if initialize_response['status'] > 0:
@@ -11,7 +12,14 @@ func _ready():
 		steam_enabled = false
 	else:
 		steam_enabled = true
-		steam_username = Steam.getPersonaName()
+
+func get_steam_username():
+	if !steam_enabled:
+		return ""
+	return Steam.getPersonaName()
 
 func _process(_delta: float) -> void:
+	if !steam_enabled:
+		set_process(false)
+		return
 	Steam.run_callbacks()
